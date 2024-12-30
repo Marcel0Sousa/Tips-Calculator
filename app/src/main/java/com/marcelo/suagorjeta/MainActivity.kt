93468b2b279aa63a6dd0f69d5fe5024d2f5e1d8e
Marcel0Sousa <tchelospt@gmail.com>
2024-12-30 14:35:48 -0300

    Utilizando o ViewBinding

--- app/src/main/java/com/marcelo/suagorjeta/MainActivity.kt
+++ app/src/main/java/com/marcelo/suagorjeta/MainActivity.kt
@@ -1,14 +1,27 @@
 package com.marcelo.suagorjeta
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.snackbar.Snackbar
+import com.marcelo.suagorjeta.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.btnCalcular.setOnClickListener {
+            Snackbar.make(binding.btnCalcular, "Teste", Snackbar.LENGTH_SHORT).show()
+        }
     }
 }
